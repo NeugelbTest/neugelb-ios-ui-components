feaee45bd0f1684cafd657293e37f70e5eac3406
Matt Moayeri <notron021@gmail.com>
2025-03-09 23:00:24 +0100

    Added some utility on preview

---
+++ Sources/Private/Extensions/Preview+Extensions.swift
@@ -0,0 +1,36 @@
+import NeugelbColors
+import NeugelbFonts
+import SwiftUI
+
+enum PreviewAsset {
+
+    static var tapAction: ButtonActionHandler {{ print("Tapped") }}
+    static var textHandler: ((String) -> Void) {{ _ in }}
+
+    static var asyncTapAction: ButtonAsyncActionHandler {{
+        try? await Task.sleep(nanoseconds: 3000000000)
+        print("Tapped")
+    }}
+
+    static var loremIpsum: String {
+        // swiftlint:disable:next line_length
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan tempus iaculis. Cras a eleifend ex. Sed cursus aliquam ante. Praesent volutpat ullamcorper risus ut rhoncus. Quisque quis lobortis mi, a eleifend enim. Cras malesuada turpis eu mauris euismod, at interdum felis cursus. Morbi varius facilisis metus, id fermentum urna. Curabitur lorem lectus, congue eu tristique quis, placerat sed est."
+    }
+
+}
+
+extension View {
+
+    func prepareForPreview(
+        backgroundColor backgroundColorAsset: ColorAsset? = .grey90
+    ) -> some View {
+        ZStack {
+            backgroundColorAsset?.color.ignoresSafeArea()
+            self
+        }
+        .onLoad {
+            NeugelbFonts.configureFonts()
+        }
+    }
+
+}
